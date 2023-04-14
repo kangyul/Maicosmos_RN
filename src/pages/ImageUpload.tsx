7000dd3243de147b6083aa5ccc7631c3bf399fd1
kangyul <yulkang2019@gmail.com>
2023-04-14 17:23:42 +0900

    feat: add image cropper functionality

--- src/pages/ImageUpload.tsx
+++ src/pages/ImageUpload.tsx
@@ -1,80 +1,159 @@
-import axios, {AxiosError} from 'axios';
-import React, {useState} from 'react';
-import {View, Button, Alert, Image, Platform} from 'react-native';
+import React, {useCallback, useRef, useState} from 'react';
 import {
-  ImagePickerResponse,
-  launchImageLibrary,
-} from 'react-native-image-picker';
+  Button,
+  Alert,
+  Image,
+  Text,
+  StyleSheet,
+  View,
+  TextInput,
+  Platform,
+} from 'react-native';
+import axios, {AxiosError} from 'axios';
+import ImagePicker from 'react-native-image-crop-picker';
+import ImageResizer from 'react-native-image-resizer';
+import {createThumbnail} from 'react-native-create-thumbnail';
 import {useSelector} from 'react-redux';
 import {RootState} from '../store/reducer';
 import Video from 'react-native-video';
+import DismissKeyboardView from '../components/DismissKeyboardView';
 
 function ImageUpload() {
-  // const [name, setName] = useState('');
-  // const [title, setTitle] = useState('');
-  const [photo, setPhoto] = useState<ImagePickerResponse | null>(null);
   const [isPhoto, setIsPhoto] = useState(true);
+  const [preview, setPreview] = useState<{uri: string}>();
+  const [image, setImage] = useState<{
+    uri: string;
+    name: string;
+    type: string;
+  }>();
+  const [video, setVideo] = useState<{
+    uri: string;
+    name: string;
+    type: string;
+  }>();
+  const [thumbnail, setThumbnail] = useState<{
+    uri: string;
+    type: string;
+  }>();
   const userId = useSelector((state: RootState) => state.user.email);
   const name = useSelector((state: RootState) => state.user.name);
+  const [title, setTitle] = useState('');
+  const [description, setDescription] = useState('');
+  const titleRef = useRef<TextInput | null>(null);
+  const descriptionRef = useRef<TextInput | null>(null);
 
-  const handleChoosePhoto = () => {
-    launchImageLibrary(
-      {
-        mediaType: 'mixed',
-        maxWidth: 512,
-        maxHeight: 512,
-        includeBase64: true,
-      },
-      res => {
-        if (res.didCancel) return;
-        setPhoto(res);
-        if (res.assets[0].type?.includes('video')) {
-          setIsPhoto(false);
-        } else {
-          setIsPhoto(true);
-        }
-      },
-    );
-    // console.log(photo.assets[0].uri);
-  };
+  const onChangeTitle = useCallback(text => {
+    setTitle(text);
+  }, []);
 
-  const handleUpload = async () => {
-    // if (!name || !name.trim()) {
-    //   return Alert.alert('알림', '작가 이름을 입력해주세요.');
-    // }
-    // if (!title || !title.trim()) {
-    //   return Alert.alert('알림', '작품 제목을 입력해주세요.');
-    // }
-    try {
-      const data = new FormData();
-      data.append('photo', {
-        name: photo.assets[0].fileName,
-        type: photo.assets[0].type,
-        uri:
-          Platform.OS === 'android'
-            ? photo.assets[0].uri
-            : photo.assets[0].uri.replace('file://', ''),
+  const onChangeDescription = useCallback(text => {
+    setDescription(text);
+  }, []);
+
+  const onResponse = useCallback(async response => {
+    console.log(response.width, response.height, response.exif);
+    setPreview({uri: `data:${response.mime};base64,${response.data}`});
+    const orientation = (response.exif as any)?.Orientation;
+    console.log('orientation', orientation);
+    return ImageResizer.createResizedImage(
+      response.path,
+      600,
+      600,
+      response.mime.includes('jpeg') ? 'JPEG' : 'PNG',
+      100,
+      0,
+    ).then(r => {
+      console.log(r.uri, r.name);
+
+      setImage({
+        uri: r.uri,
+        name: r.name,
+        type: response.mime,
       });
 
-      console.log(
-        '기존 파일 위치:' + photo.assets[0].uri.replace('file://', ''),
-      );
+      setIsPhoto(true);
+    });
+  }, []);
 
-      data.append('userId', userId);
-      data.append('name', name);
+  const onChangeImageFile = useCallback(() => {
+    return ImagePicker.openPicker({
+      includeExif: true,
+      includeBase64: true,
+      cropping: true,
+      mediaType: 'any',
+    })
+      .then(onResponse)
+      .catch(console.log);
+  }, [onResponse]);
 
-      // http 메서드: get, put, patch, post, delete, head, options
+  const createVideoThumbnail = useCallback(async response => {
+    setVideo({
+      uri: response.path,
+      name: response.filename,
+      type: response.mime,
+    });
+    createThumbnail({
+      url: response.path,
+      timeStamp: 0,
+    })
+      .then(thumb => {
+        setThumbnail({
+          uri: thumb.path,
+          type: thumb.mime,
+        });
+      })
+      .catch(err => console.log({err}));
+  }, []);
+
+  const onChangeVideoFile = useCallback(() => {
+    setIsPhoto(false);
+    ImagePicker.openPicker({
+      mediaType: 'video',
+    }).then(createVideoThumbnail);
+  }, [createVideoThumbnail]);
+
+  const onFileUpload = useCallback(async () => {
+    if (!image && !video) {
+      Alert.alert('알림', '파일을 업로드 해주세요.');
+      return;
+    }
+
+    const formData = new FormData();
+    formData.append('userId', userId);
+    formData.append('name', name);
+    if (isPhoto) {
+      formData.append('photo', {
+        name: image!.name,
+        type: image!.type,
+        uri: image!.uri,
+      });
+    } else {
+      // formData.append('media', {
+      //   name: video!.name,
+      //   type: video!.type,
+      //   uri: video!.uri,
+      // });
+      // 썸네일
+
+      console.log('썸네일 타입' + thumbnail?.type);
+
+      formData.append('photo', {
+        type: thumbnail.type,
+        uri: thumbnail.uri,
+      });
+    }
+
+    try {
       const response = await axios.post(
         'https://maicosmos.com/RN/image.php',
-        data,
+        formData,
         {
           headers: {'content-type': 'multipart/form-data'},
         },
       );
-      console.log('파일명: ' + response.data.file);
-      console.log('메세지: ' + response.data.message);
-      console.log('Upload flag: ' + response.data.flag);
-      // console.log('파일 타입: ' + response.data.fileType);
+
+      console.log('파일 이름: ' + response.data.name);
+
       Alert.alert('알림:', '이미지/동영상 업로드 완료!');
     } catch (error) {
       const errorResponse = (error as AxiosError).response;
@@ -83,31 +162,75 @@ function ImageUpload() {
         Alert.alert('알림', errorResponse.data.message);
       }
     } finally {
-      setPhoto(null);
+      // setPhoto(null);
     }
-  };
-
+  }, [image, video]);
   return (
-    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-      {photo && (
+    <DismissKeyboardView>
+      {(image || video) && (
         <React.Fragment>
           {isPhoto ? (
             <Image
-              source={{uri: photo.assets[0].uri}}
-              style={{width: 300, height: 300}}
+              source={{uri: image.uri}}
+              style={{width: 300, height: 300, alignSelf: 'center'}}
             />
           ) : (
             <Video
-              source={{uri: photo.assets[0].uri}}
-              style={{width: 300, height: 300}}
+              source={{uri: video.uri}}
+              style={{width: 300, height: 300, alignSelf: 'center'}}
             />
           )}
-          <Button title="Upload" onPress={handleUpload} />
+          <Button title="Upload" onPress={onFileUpload} />
         </React.Fragment>
       )}
-      <Button title="이미지/동영상을 선택하세요." onPress={handleChoosePhoto} />
-    </View>
+      <Button title="이미지 선택" onPress={onChangeImageFile} />
+      <Button title="동영상 선택" onPress={onChangeVideoFile} />
+      <View style={styles.inputWrapper}>
+        <Text style={styles.label}>작품제목</Text>
+        <TextInput
+          style={styles.textInput}
+          onChangeText={onChangeTitle}
+          placeholder="작품제목"
+          placeholderTextColor="#666"
+          value={title}
+          returnKeyType="next"
+          clearButtonMode="while-editing"
+          ref={titleRef}
+          onSubmitEditing={() => descriptionRef.current?.focus()}
+          blurOnSubmit={false}
+        />
+      </View>
+      <View style={styles.inputWrapper}>
+        <Text style={styles.label}>작품설명</Text>
+        <TextInput
+          style={styles.textInput}
+          placeholder="작품설명"
+          placeholderTextColor="#666"
+          onChangeText={onChangeDescription}
+          value={description}
+          returnKeyType="send"
+          clearButtonMode="while-editing"
+          ref={descriptionRef}
+          onSubmitEditing={onFileUpload}
+        />
+      </View>
+    </DismissKeyboardView>
   );
 }
 
 export default ImageUpload;
+
+const styles = StyleSheet.create({
+  textInput: {
+    padding: 5,
+    borderBottomWidth: StyleSheet.hairlineWidth,
+  },
+  inputWrapper: {
+    padding: 20,
+  },
+  label: {
+    fontWeight: 'bold',
+    fontSize: 16,
+    marginBottom: 20,
+  },
+});
