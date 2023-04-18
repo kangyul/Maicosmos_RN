fa76d0f9d7e22fca4be78aa084c6d54591a79977
kangyul <yulkang2019@gmail.com>
2023-04-18 17:53:28 +0900

    feat: add editprofile UI

--- src/pages/EditProfile.tsx
+++ src/pages/EditProfile.tsx
@@ -1,21 +1,218 @@
-import React from 'react';
-import {Button, StyleSheet, Text, View} from 'react-native';
+import React, {useCallback, useRef, useState} from 'react';
+import {
+  ActivityIndicator,
+  Button,
+  Platform,
+  Pressable,
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+} from 'react-native';
+import DismissKeyboardView from '../components/DismissKeyboardView';
 
 function Edit() {
+  const [loading, setLoading] = useState(false);
+  const [email, setEmail] = useState('');
+  const [id, setId] = useState('');
+  const [name, setName] = useState('');
+  const [nick, setNick] = useState('');
+  const [password, setPassword] = useState('');
+  const [password2, setPassword2] = useState('');
+  const emailRef = useRef<TextInput | null>(null);
+  const idRef = useRef<TextInput | null>(null);
+  const nameRef = useRef<TextInput | null>(null);
+  const passwordRef = useRef<TextInput | null>(null);
+  const password2Ref = useRef<TextInput | null>(null);
+  const nickRef = useRef<TextInput | null>(null);
+
+  const onChangeEmail = useCallback(text => {
+    setEmail(text.trim());
+  }, []);
+  const onChangeId = useCallback(text => {
+    setId(text.trim());
+  }, []);
+  const onChangeName = useCallback(text => {
+    setName(text.trim());
+  }, []);
+  const onChangePassword = useCallback(text => {
+    setPassword(text.trim());
+  }, []);
+  const onChangePassword2 = useCallback(text => {
+    setPassword2(text.trim());
+  }, []);
+  const onChangeNick = useCallback(text => {
+    setNick(text.trim());
+  }, []);
+
+  const onSubmit = useCallback(async () => {}, []);
+
+  const canGoNext = email && id && name && password && nick && password2;
+
   return (
-    <View style={styles.container}>
-      <Text>프로필 변경</Text>
-      <Button title="Click Here" onPress={() => alert('Button clicked!')} />
-    </View>
+    <DismissKeyboardView style={styles.backGround}>
+      <View style={styles.inputWrapper}>
+        <TextInput
+          style={styles.textInput}
+          onChangeText={onChangeEmail}
+          placeholder="이메일"
+          placeholderTextColor="#666"
+          textContentType="emailAddress"
+          value={email}
+          returnKeyType="next"
+          clearButtonMode="while-editing"
+          ref={emailRef}
+          onSubmitEditing={() => idRef.current?.focus()}
+          blurOnSubmit={false}
+        />
+      </View>
+      <View style={styles.inputWrapper}>
+        <TextInput
+          style={styles.textInput}
+          onChangeText={onChangeId}
+          placeholder="아이디"
+          placeholderTextColor="#666"
+          textContentType="username"
+          value={id}
+          returnKeyType="next"
+          clearButtonMode="while-editing"
+          ref={idRef}
+          onSubmitEditing={() => nameRef.current?.focus()}
+          blurOnSubmit={false}
+        />
+      </View>
+      <View style={styles.inputWrapper}>
+        <TextInput
+          style={styles.textInput}
+          placeholder="이름"
+          placeholderTextColor="#666"
+          onChangeText={onChangeName}
+          value={name}
+          textContentType="name"
+          returnKeyType="next"
+          clearButtonMode="while-editing"
+          ref={nameRef}
+          onSubmitEditing={() => nickRef.current?.focus()}
+          blurOnSubmit={false}
+        />
+      </View>
+      <View style={styles.inputWrapper}>
+        <TextInput
+          style={styles.textInput}
+          placeholder="닉네임"
+          placeholderTextColor="#666"
+          onChangeText={onChangeNick}
+          value={nick}
+          textContentType="nickname"
+          returnKeyType="next"
+          clearButtonMode="while-editing"
+          ref={nickRef}
+          onSubmitEditing={() => passwordRef.current?.focus()}
+          blurOnSubmit={false}
+        />
+      </View>
+      <View style={styles.inputWrapper}>
+        <TextInput
+          style={styles.textInput}
+          placeholder="비밀번호"
+          placeholderTextColor="#666"
+          onChangeText={onChangePassword}
+          value={password}
+          keyboardType={Platform.OS === 'android' ? 'default' : 'ascii-capable'}
+          secureTextEntry={true}
+          textContentType="oneTimeCode"
+          blurOnSubmit={false}
+          returnKeyType="next"
+          clearButtonMode="while-editing"
+          ref={passwordRef}
+          onSubmitEditing={() => password2Ref.current?.focus()}
+        />
+      </View>
+      <View style={styles.inputWrapper}>
+        <TextInput
+          style={styles.textInput}
+          placeholder="비밀번호 확인"
+          placeholderTextColor="#666"
+          onChangeText={onChangePassword2}
+          value={password2}
+          keyboardType={Platform.OS === 'android' ? 'default' : 'ascii-capable'}
+          secureTextEntry={true}
+          textContentType="oneTimeCode"
+          returnKeyType="send"
+          clearButtonMode="while-editing"
+          ref={password2Ref}
+          onSubmitEditing={onSubmit}
+        />
+      </View>
+      <View style={styles.buttonZone}>
+        <Pressable
+          style={
+            canGoNext
+              ? StyleSheet.compose(styles.loginButton, styles.loginButtonActive)
+              : styles.loginButton
+          }
+          disabled={!canGoNext || loading}
+          onPress={onSubmit}>
+          {loading ? (
+            <ActivityIndicator color="blue" />
+          ) : (
+            <Text style={styles.loginButtonText}>회원가입</Text>
+          )}
+        </Pressable>
+      </View>
+    </DismissKeyboardView>
   );
 }
 
 export default Edit;
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
+  backGround: {
+    backgroundColor: '#fff',
+  },
+  signInImage: {
+    width: 124,
+    height: 124,
+    alignSelf: 'center',
+    marginTop: '10%',
+    marginBottom: '5%',
+  },
+  textInput: {
+    paddingHorizontal: 15,
+    borderWidth: 2,
+    borderRadius: 8,
+    borderColor: 'rgba(224, 224, 224, 1)',
+    height: 50,
+    fontSize: 15,
+  },
+  inputWrapper: {
+    paddingHorizontal: 20,
+    paddingVertical: 5,
+  },
+  label: {
+    fontWeight: 'bold',
+    fontSize: 16,
+    marginBottom: 20,
+  },
+  buttonZone: {
     alignItems: 'center',
-    justifyContent: 'center',
+  },
+  loginButton: {
+    backgroundColor: 'gray',
+    paddingHorizontal: 20,
+    paddingVertical: 16,
+    borderRadius: 5,
+    marginBottom: 20,
+    marginTop: 10,
+    width: '90%',
+  },
+  loginButtonActive: {
+    backgroundColor: 'rgba(0, 196, 255, 1)',
+  },
+  loginButtonText: {
+    color: 'white',
+    fontSize: 15,
+    fontWeight: '700',
+    textAlign: 'center',
   },
 });
