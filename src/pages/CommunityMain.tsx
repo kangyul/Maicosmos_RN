694349c8dad7767bfd4fda3d0e3cfa3990059c8b
kangyul <yulkang2019@gmail.com>
2023-04-24 09:31:01 +0900

    feat: add community main page
    커뮤니티 메인 더미 페이지 추가

---
+++ src/pages/CommunityMain.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+
+function CommunityMain() {
+  return (
+    <View style={{alignItems: 'center', justifyContent: 'center'}}>
+      <Text>커뮤니티 메인 페이지입니다.</Text>
+    </View>
+  );
+}
+
+export default CommunityMain;
