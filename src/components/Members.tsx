bc3bc88e5213295d1c7b1d87c1f969d55ebc564b
kangyul <yulkang2019@gmail.com>
2023-04-25 16:12:16 +0900

    add members component in community main page

--- src/components/Members.tsx
+++ src/components/Members.tsx
@@ -1,12 +1,44 @@
 import React from 'react';
-import {Image, ScrollView, StyleSheet, Text, View} from 'react-native';
+import {
+  Image,
+  Platform,
+  ScrollView,
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
 
-function Members() {
+function Members(props) {
+  const members = props.members;
+
   return (
     <ScrollView showsVerticalScrollIndicator={false} style={styles.scrollView}>
       <View style={styles.tabTitle}>
         <Text style={styles.tabTitleText}>구성원</Text>
       </View>
+      {members && (
+        <View>
+          {members.map(member => (
+            <View
+              key={member.gm_id}
+              style={{
+                flex: 1,
+                flexDirection: 'row',
+                padding: 10,
+                marginBottom: 2,
+                backgroundColor: 'rgba(255,255,255,1)',
+                borderRadius: 16,
+                alignItems: 'center',
+              }}>
+              <Image style={styles.memberImg} source={{uri: member.img}} />
+              <View>
+                <Text style={styles.nick}>{member.nick}</Text>
+                <Text style={styles.name}>{member.name}</Text>
+              </View>
+            </View>
+          ))}
+        </View>
+      )}
     </ScrollView>
   );
 }
@@ -35,5 +67,21 @@ const styles = StyleSheet.create({
   tabTitleText: {
     fontWeight: 'bold',
     fontSize: 18,
+  },
+  memberImg: {
+    height: 30,
+    width: 30,
+    borderRadius: 100,
+    marginRight: 10,
+  },
+  name: {
+    fontWeight: '700',
+    fontSize: 18,
+    marginBottom: 5,
+  },
+  nick: {
+    fontWeight: '700',
+    fontSize: 18,
+    marginBottom: 5,
   },
 });
