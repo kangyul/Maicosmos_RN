c8e671b0e490c3e20ddc7770766faf412ff4986b
kangyul <yulkang2019@gmail.com>
2023-05-01 09:22:03 +0900

    feat: 홈, 알림 스크린 추가

---
+++ src/pages/Home.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+
+function Home() {
+  return (
+    <View>
+      <Text>홈스크린</Text>
+    </View>
+  );
+}
+
+export default Home;
--- src/pages/HomeScreen.tsx
+++ src/pages/HomeScreen.tsx
@@ -8,6 +8,8 @@ import Settings from './Settings';
 import ImageUpload from './ImageUpload';
 import PersonalGallery from './PersonalGallery';
 import EditProfile from './EditProfile';
+import Home from './Home';
+import Notification from './Notification';
 
 const Tab = createBottomTabNavigator();
 const Stack = createNativeStackNavigator();
@@ -53,13 +55,21 @@ function HomeScreen() {
         tabBarIcon: ({focused, size, color}) => {
           let iconName;
           if (route.name === 'CommunityStackScreen') {
-            iconName = focused ? 'ios-home' : 'ios-home-outline';
+            iconName = focused
+              ? 'md-people-circle'
+              : 'md-people-circle-outline';
           } else if (route.name === 'ImageUpload') {
             iconName = focused ? 'ios-image' : 'ios-image-outline';
           } else if (route.name === 'SettingsStackScreen') {
             iconName = focused
               ? 'ios-person-circle'
               : 'ios-person-circle-outline';
+          } else if (route.name === 'Home') {
+            iconName = focused ? 'ios-home' : 'ios-home-outline';
+          } else if (route.name === 'Notification') {
+            iconName = focused
+              ? 'md-notifications'
+              : 'md-notifications-outline';
           }
 
           size = 25;
@@ -67,11 +77,13 @@ function HomeScreen() {
           return <Ionic name={iconName} size={size} color={color} />;
         },
       })}>
+      <Tab.Screen name="Home" component={Home} />
       <Tab.Screen
         name="CommunityStackScreen"
         component={CommunityStackScreen}
       />
       <Tab.Screen name="ImageUpload" component={ImageUpload} />
+      <Tab.Screen name="Notification" component={Notification} />
       <Tab.Screen name="SettingsStackScreen" component={SettingsStackScreen} />
     </Tab.Navigator>
   );
