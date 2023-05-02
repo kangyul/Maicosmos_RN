c385718b3cf57315b91038d9897f23eb369df0ad
kangyul <yulkang2019@gmail.com>
2023-05-02 17:37:08 +0900

    feat: firebase android test

--- App.tsx
+++ App.tsx
@@ -2,8 +2,19 @@ import * as React from 'react';
 import {Provider} from 'react-redux';
 import store from './src/store';
 import AppInner from './AppInner';
+import {useEffect} from 'react';
+import messaging from '@react-native-firebase/messaging';
+import {Alert} from 'react-native';
 
 function App() {
+  useEffect(() => {
+    const unsubscribe = messaging().onMessage(async remoteMessage => {
+      Alert.alert('A new FCM message arrived!', JSON.stringify(remoteMessage));
+    });
+
+    return unsubscribe;
+  }, []);
+
   return (
     <Provider store={store}>
       <AppInner />
