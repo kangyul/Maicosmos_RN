6cd3f192c13b6b14856b5ebf73d5760bb9d56204
kangyul <yulkang2019@gmail.com>
2023-05-16 14:16:27 +0900

    style: 프로필 편집 UI

--- src/pages/EditProfile.tsx
+++ src/pages/EditProfile.tsx
@@ -1,10 +1,8 @@
-import React, {useCallback, useRef, useState} from 'react';
+import React, {useCallback, useEffect, useRef, useState} from 'react';
 import {
-  ActivityIndicator,
   Alert,
-  ImageBackground,
-  Platform,
-  Pressable,
+  Button,
+  Image,
   StyleSheet,
   Text,
   TextInput,
@@ -20,23 +18,21 @@ import userSlice from '../slices/user';
 import ImagePicker from 'react-native-image-crop-picker';
 import ImageResizer from 'react-native-image-resizer';
 
-import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
-
 function Edit({navigation}) {
   const dispatch = useAppDispatch();
 
   const idDefault = useSelector((state: RootState) => state.user.id);
+  const nameDefault = useSelector((state: RootState) => state.user.name);
   const nickDefault = useSelector((state: RootState) => state.user.nick);
   const aboutDefault = useSelector((state: RootState) => state.user.desc);
   const imgDefault = useSelector((state: RootState) => state.user.img);
 
   const [loading, setLoading] = useState(false);
-  const [nick, setNick] = useState(nickDefault);
-  const [password, setPassword] = useState('');
-  const [password2, setPassword2] = useState('');
-  const [about, setAbout] = useState(aboutDefault);
-  const passwordRef = useRef<TextInput | null>(null);
-  const password2Ref = useRef<TextInput | null>(null);
+  const [name, setName] = useState<string>(nameDefault);
+  const [nick, setNick] = useState<string>(nickDefault);
+  const [about, setAbout] = useState<string>(aboutDefault);
+
+  const nameRef = useRef<TextInput | null>(null);
   const nickRef = useRef<TextInput | null>(null);
 
   const [profilePic, setProfileImage] = useState(imgDefault);
@@ -47,16 +43,13 @@ function Edit({navigation}) {
     type: string;
   }>();
 
-  const onChangePassword = useCallback(text => {
-    setPassword(text.trim());
+  const onChangeName = useCallback((text: string) => {
+    setName(text.trim());
   }, []);
-  const onChangePassword2 = useCallback(text => {
-    setPassword2(text.trim());
-  }, []);
-  const onChangeNick = useCallback(text => {
+  const onChangeNick = useCallback((text: string) => {
     setNick(text.trim());
   }, []);
-  const onChangeAbout = useCallback(text => {
+  const onChangeAbout = useCallback((text: string) => {
     setAbout(text.trim());
   }, []);
 
@@ -65,14 +58,8 @@ function Edit({navigation}) {
       return;
     }
 
-    if (password !== password2) {
-      Alert.alert('알림', '패스워드가 일치하지 않습니다.');
-      return;
-    }
-
     const formData = new FormData();
     formData.append('userId', idDefault);
-    formData.append('password', password);
     formData.append('nick', nick);
     formData.append('about', about);
 
@@ -120,11 +107,7 @@ function Edit({navigation}) {
     navigation,
     nick,
     nickDefault,
-    password,
-    password2,
   ]);
-
-  const canGoNext = password && nick && password2;
 
   const onResponse = useCallback(async response => {
     console.log(response.width, response.height, response.exif);
@@ -162,10 +145,29 @@ function Edit({navigation}) {
       .catch(console.log);
   }, [onResponse]);
 
+  useEffect(() => {
+    navigation.setOptions({
+      headerRight: () => (
+        <TouchableOpacity
+          onPress={() => console.log('buttontton')}
+          title="ChangeUserInfo">
+          <Text style={styles.ChangeUserInfo}>완료</Text>
+        </TouchableOpacity>
+      ),
+    });
+  }, [navigation]);
+
   return (
     <DismissKeyboardView style={styles.backGround}>
-      <View style={{alignItems: 'center', marginVertical: 20}}>
-        <TouchableOpacity onPress={onChangeImageFile}>
+      <View style={{alignItems: 'center', marginTop: 40, marginBottom: 20}}>
+        <Image
+          style={{width: 120, height: 120, borderRadius: 75, marginBottom: 10}}
+          source={{uri: profilePic}}
+        />
+        <TouchableOpacity style={styles.editBtn} onPress={onChangeImageFile}>
+          <Text style={styles.editBtnText}>변경</Text>
+        </TouchableOpacity>
+        {/* <TouchableOpacity onPress={onChangeImageFile}>
           <View
             style={{
               height: 200,
@@ -190,67 +192,53 @@ function Edit({navigation}) {
               </View>
             </ImageBackground>
           </View>
-        </TouchableOpacity>
+        </TouchableOpacity> */}
       </View>
-      <View style={styles.inputWrapper}>
-        <TextInput
-          style={styles.textInput}
-          placeholder="닉네임"
-          placeholderTextColor="#666"
-          onChangeText={onChangeNick}
-          value={nick}
-          textContentType="nickname"
-          returnKeyType="next"
-          clearButtonMode="while-editing"
-          ref={nickRef}
-          onSubmitEditing={() => passwordRef.current?.focus()}
-          blurOnSubmit={false}
-        />
+      <View style={styles.inputContainer}>
+        <View style={styles.inputWrapper}>
+          <Text style={styles.inputTitle}>이름</Text>
+          <TextInput
+            style={styles.textInput}
+            placeholder="이름"
+            placeholderTextColor="#666"
+            onChangeText={onChangeName}
+            value={name}
+            returnKeyType="next"
+            clearButtonMode="while-editing"
+            ref={nameRef}
+            onSubmitEditing={() => nickRef.current?.focus()}
+            blurOnSubmit={false}
+          />
+        </View>
+        <View style={styles.inputWrapper}>
+          <Text style={styles.inputTitle}>닉네임</Text>
+          <TextInput
+            style={styles.textInput}
+            placeholder="닉네임"
+            placeholderTextColor="#666"
+            onChangeText={onChangeNick}
+            value={nick}
+            textContentType="nickname"
+            returnKeyType="next"
+            clearButtonMode="while-editing"
+            ref={nickRef}
+            onSubmitEditing={() => passwordRef.current?.focus()}
+            blurOnSubmit={false}
+          />
+        </View>
+        <View style={styles.inputWrapper}>
+          <Text style={styles.inputTitle}>자기 소개글</Text>
+          <TextInput
+            style={styles.textInput}
+            placeholder="자기소개 글"
+            placeholderTextColor="#666"
+            clearButtonMode="while-editing"
+            value={about}
+            onChangeText={onChangeAbout}
+          />
+        </View>
       </View>
-      <View style={{paddingHorizontal: 20, paddingVertical: 5}}>
-        <TextInput
-          style={styles.textInput}
-          placeholder="자기소개 글"
-          placeholderTextColor="#666"
-          clearButtonMode="while-editing"
-          value={about}
-          onChangeText={onChangeAbout}
-        />
-      </View>
-      <View style={styles.inputWrapper}>
-        <TextInput
-          style={styles.textInput}
-          placeholder="비밀번호"
-          placeholderTextColor="#666"
-          onChangeText={onChangePassword}
-          value={password}
-          keyboardType={Platform.OS === 'android' ? 'default' : 'ascii-capable'}
-          secureTextEntry={true}
-          textContentType="oneTimeCode"
-          blurOnSubmit={false}
-          returnKeyType="next"
-          clearButtonMode="while-editing"
-          ref={passwordRef}
-          onSubmitEditing={() => password2Ref.current?.focus()}
-        />
-      </View>
-      <View style={styles.inputWrapper}>
-        <TextInput
-          style={styles.textInput}
-          placeholder="비밀번호 확인"
-          placeholderTextColor="#666"
-          onChangeText={onChangePassword2}
-          value={password2}
-          keyboardType={Platform.OS === 'android' ? 'default' : 'ascii-capable'}
-          secureTextEntry={true}
-          textContentType="oneTimeCode"
-          returnKeyType="send"
-          clearButtonMode="while-editing"
-          ref={password2Ref}
-          onSubmitEditing={onSubmit}
-        />
-      </View>
-      <View style={styles.buttonZone}>
+      {/* <View style={styles.buttonZone}>
         <Pressable
           style={
             canGoNext
@@ -265,7 +253,7 @@ function Edit({navigation}) {
             <Text style={styles.loginButtonText}>정보수정</Text>
           )}
         </Pressable>
-      </View>
+      </View> */}
     </DismissKeyboardView>
   );
 }
@@ -278,15 +266,22 @@ const styles = StyleSheet.create({
   },
   textInput: {
     paddingHorizontal: 15,
-    borderWidth: 2,
-    borderRadius: 8,
+    borderWidth: 1.5,
+    borderRadius: 5,
     borderColor: 'rgba(224, 224, 224, 1)',
     height: 50,
-    fontSize: 15,
+    fontSize: 17,
+  },
+  inputContainer: {
+    paddingHorizontal: 30,
   },
   inputWrapper: {
-    paddingHorizontal: 20,
     paddingVertical: 5,
+    marginBottom: 10,
+  },
+  inputTitle: {
+    fontSize: 15,
+    marginBottom: 8,
   },
   label: {
     fontWeight: 'bold',
@@ -314,17 +309,22 @@ const styles = StyleSheet.create({
     fontWeight: '700',
     textAlign: 'center',
   },
-  cameraIconView: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+  editBtn: {
+    backgroundColor: '#f5f5f5',
+    paddingHorizontal: 20,
+    paddingVertical: 10,
+    borderRadius: 8,
+    marginTop: 10,
+    alignSelf: 'center',
   },
-  cameraIcon: {
-    opacity: 0.7,
-    alignItems: 'center',
-    justifyContent: 'center',
-    borderWidth: 1,
-    borderColor: '#fff',
-    borderRadius: 10,
+  editBtnText: {
+    color: 'black',
+    fontSize: 15,
+    fontWeight: '400',
+  },
+  ChangeUserInfo: {
+    fontSize: 19,
+    color: '#AA74FF',
+    fontWeight: '500',
   },
 });
