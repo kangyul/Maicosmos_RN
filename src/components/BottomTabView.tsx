6c60f3efb13fb93f39f771a36973e33f62cd7c2c
kangyul <yulkang2019@gmail.com>
2023-05-17 17:17:54 +0900

    style: 탭 아이콘에서 텍스트로 변경

--- src/components/BottomTabView.tsx
+++ src/components/BottomTabView.tsx
@@ -1,7 +1,5 @@
 import React from 'react';
-import {ScrollView, Text, View} from 'react-native';
 import {createMaterialTopTabNavigator} from '@react-navigation/material-top-tabs';
-import Ionic from 'react-native-vector-icons/Ionicons';
 import Galleries from './Galleries';
 import Boards from './Boards';
 import Members from './Members';
@@ -12,41 +10,29 @@ function BottomTabView(props) {
   return (
     <Tab.Navigator
       screenOptions={({route}) => ({
-        tabBarShowLabel: false,
         tabBarIndicatorStyle: {
           backgroundColor: 'black',
-          height: 1.5,
+          height: 1,
         },
-        tabBarIcon: ({focused, color}) => {
-          let iconName;
-          if (route.name === 'Galleries') {
-            iconName = focused ? 'ios-images' : 'ios-images-sharp';
-            color = focused ? 'black' : 'gray';
-          } else if (route.name === 'Boards') {
-            iconName = focused ? 'ios-clipboard' : 'ios-clipboard-outline';
-            color = focused ? 'black' : 'gray';
-          } else if (route.name === 'Members') {
-            iconName = focused ? 'ios-person' : 'ios-person-outline';
-            color = focused ? 'black' : 'gray';
-          }
-
-          return <Ionic name={iconName} color={color} size={22} />;
+        tabBarLabelStyle: {
+          fontSize: 15,
+          fontWeight: 'bold',
         },
       })}>
       <Tab.Screen
-        name="Galleries"
+        name="갤러리"
         children={() => <Galleries galleries={props.galleries} />}
       />
       {/* <Tab.Screen name="Boards" component={Boards} /> */}
       <Tab.Screen
-        name="Boards"
+        name="게시판"
         children={() => (
           <Boards boardAN={props.boardAN} boardFR={props.boardFR} />
         )}
       />
       {/* <Tab.Screen name="Members" component={Members} /> */}
       <Tab.Screen
-        name="Members"
+        name="구성원"
         children={() => <Members members={props.members} />}
       />
     </Tab.Navigator>
