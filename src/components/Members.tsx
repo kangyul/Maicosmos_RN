a6c18c3bc6acd2936d73b691c5132740d2eaa5f2
kangyul <yulkang2019@gmail.com>
2023-05-18 14:37:05 +0900

    add DismissKeyboardView

--- src/components/Members.tsx
+++ src/components/Members.tsx
@@ -9,73 +9,76 @@ import {
   TouchableOpacity,
   View,
 } from 'react-native';
+import DismissKeyboardView from './DismissKeyboardView';
 
 function Members(props) {
   const members = props.members;
 
   return (
     <ScrollView showsVerticalScrollIndicator={false} style={styles.scrollView}>
-      <View style={{padding: 20}}>
-        <View style={{flex: 1, left: 0, right: 0}}>
-          <View style={{flexDirection: 'row', alignItems: 'center'}}>
-            <Image
-              source={require('../../assets/image/search.png')}
-              style={{
-                height: 25,
-                width: 25,
-                position: 'absolute',
-                zIndex: 3,
-                left: 15,
-              }}
-            />
-            <TextInput
-              style={{
-                flex: 1,
-                height: 40,
-                borderRadius: 20,
-                paddingHorizontal: 10,
-                backgroundColor: '#f5f5f5',
-                fontSize: 16,
-                color: '#111',
-                paddingLeft: 50,
-              }}
-              placeholderTextColor={'#757575'}
-              placeholder="검색"
-              // onChangeText={onChangeText}
-            />
-          </View>
-        </View>
-        <Text style={styles.memberCntText}>{members.length}명</Text>
-        {members && (
-          <View>
-            {members.map(member => (
-              <View
-                key={member.gm_id}
+      <DismissKeyboardView>
+        <View style={{padding: 20}}>
+          <View style={{flex: 1, left: 0, right: 0}}>
+            <View style={{flexDirection: 'row', alignItems: 'center'}}>
+              <Image
+                source={require('../../assets/image/search.png')}
+                style={{
+                  height: 25,
+                  width: 25,
+                  position: 'absolute',
+                  zIndex: 3,
+                  left: 15,
+                }}
+              />
+              <TextInput
                 style={{
                   flex: 1,
-                  flexDirection: 'row',
-                  marginBottom: 20,
-                  backgroundColor: 'rgba(255,255,255,1)',
-                  alignItems: 'center',
-                }}>
-                <View style={{flex: 1, flexDirection: 'row'}}>
-                  <Image
-                    style={styles.memberImg}
-                    source={{uri: 'https://www.maicosmos.com' + member.img}}
-                  />
-                  <View style={{alignSelf: 'center'}}>
-                    <Text style={styles.nick}>{member.nick}</Text>
-                    <Text style={styles.name}>{member.name}</Text>
-                  </View>
-                </View>
-                <TouchableOpacity style={styles.addBtn}>
-                  <Text style={styles.addBtnText}>팔로우</Text>
-                </TouchableOpacity>
-              </View>
-            ))}
+                  height: 40,
+                  borderRadius: 20,
+                  paddingHorizontal: 10,
+                  backgroundColor: '#f5f5f5',
+                  fontSize: 16,
+                  color: '#111',
+                  paddingLeft: 50,
+                }}
+                placeholderTextColor={'#757575'}
+                placeholder="검색"
+                // onChangeText={onChangeText}
+              />
+            </View>
           </View>
-        )}
-      </View>
+          <Text style={styles.memberCntText}>{members.length}명</Text>
+          {members && (
+            <View>
+              {members.map(member => (
+                <View
+                  key={member.gm_id}
+                  style={{
+                    flex: 1,
+                    flexDirection: 'row',
+                    marginBottom: 20,
+                    backgroundColor: 'rgba(255,255,255,1)',
+                    alignItems: 'center',
+                  }}>
+                  <View style={{flex: 1, flexDirection: 'row'}}>
+                    <Image
+                      style={styles.memberImg}
+                      source={{uri: 'https://www.maicosmos.com' + member.img}}
+                    />
+                    <View style={{alignSelf: 'center'}}>
+                      <Text style={styles.nick}>{member.nick}</Text>
+                      <Text style={styles.name}>{member.name}</Text>
+                    </View>
+                  </View>
+                  <TouchableOpacity style={styles.addBtn}>
+                    <Text style={styles.addBtnText}>팔로우</Text>
+                  </TouchableOpacity>
+                </View>
+              ))}
+            </View>
+          )}
+        </View>
+      </DismissKeyboardView>
     </ScrollView>
   );
 }
